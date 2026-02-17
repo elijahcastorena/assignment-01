c9aaf60e4f3ca16e6cefb7bc6c0e5679711c7da8
Rafael Pasiona <rafaelpasiona32@gmail.com>
2026-02-16 19:04:34 -0600

    task 1: printInventory method

--- Grocery.java
+++ Grocery.java
@@ -1,19 +1,7 @@
-import java.util.Scanner;
-
-// Adds index to amount in the stock if found, otherwise prints "Item not found."
-public static void restockItem(String[] names, int[] stocks, String target, int amount) {
-    boolean found = false;
-
-    for (int i = 0; i < names.length; i++) {
-        if (names[i] != null && names[i].equalsIgnoreCase(target)) {
-            stocks[i] += amount;
-            System.out.println("Restocked " + names[i] + ". New stock: " + stocks[i]);
-            found = true;
-            break;
+public static void printInventory(String[] names, double[] prices, int[] stocks){
+    for (int i = 0; i < names.length; i++){
+        if (names[i] != null){
+            System.out.printf("%s -- $%.2f -- %d in stocks.%n", names[i], prices[i], stocks[i]);
         }
-    }
-
-    if (!found) {
-        System.out.println("Item not found.");
     }
 }
